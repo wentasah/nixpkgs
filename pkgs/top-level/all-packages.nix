2b076df97f638827d006f763002fa84466e8f3b3
Aliaksandr <grubian2@gmail.com>
2026-01-03 02:51:37 +0200

    texworks: move to by-name, modernize

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3656,8 +3656,6 @@ with pkgs;
     extraFonts = true;
   };
 
-  texworks = qt6Packages.callPackage ../applications/editors/texworks { };
-
   tiled = libsForQt5.callPackage ../applications/editors/tiled { };
 
   tinc = callPackage ../tools/networking/tinc { };
