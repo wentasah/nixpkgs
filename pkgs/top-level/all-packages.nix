f7d0aa8579b54381a6e06678b12cc6c1e6172971
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:47 +0100

    python3Packages.pywal16: migrate from pywal; 3.3.0 -> 3.8.13
    This commit was automatically generated using update-python-libraries.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3438,8 +3438,6 @@ with pkgs;
 
   pydeps = with python3Packages; toPythonApplication pydeps;
 
-  pywal = with python3Packages; toPythonApplication pywal;
-
   remarshal = with python3Packages; toPythonApplication remarshal;
 
   riseup-vpn = qt6Packages.callPackage ../tools/networking/bitmask-vpn {
