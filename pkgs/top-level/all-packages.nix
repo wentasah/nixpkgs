4fb0462eccd53802e61d6b0dc70e7d98cc06d167
Yureka <yureka@cyberchaos.dev>
2026-01-29 10:59:51 +0100

    electron-source.electron_37: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5683,10 +5683,7 @@ with pkgs;
       in
       {
         electron_36 = electron_36-bin;
-        electron_37 = getElectronPkg {
-          src = electron-source.electron_37;
-          bin = electron_37-bin;
-        };
+        electron_37 = electron_37-bin;
         electron_38 = getElectronPkg {
           src = electron-source.electron_38;
           bin = electron_38-bin;
