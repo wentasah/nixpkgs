bd2168620ab40835fad5f97fae602c999483f656
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-31 00:39:13 +0100

    assh: migrate to finalAttrs

--- pkgs/by-name/as/assh/package.nix
+++ pkgs/by-name/as/assh/package.nix
@@ -8,14 +8,14 @@
   ps,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "assh";
   version = "2.17.0";
 
   src = fetchFromGitHub {
     repo = "advanced-ssh-config";
     owner = "moul";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-X5UWQe4c+QudmXKjFKafivO/OvdBNzyutrL+CUK0olg=";
   };
 
@@ -24,7 +24,7 @@ buildGoModule rec {
   ldflags = [
     "-s"
     "-w"
-    "-X moul.io/assh/v2/pkg/version.Version=${version}"
+    "-X=moul.io/assh/v2/pkg/version.Version=${finalAttrs.version}"
   ];
 
   nativeBuildInputs = [ makeWrapper ];
@@ -44,9 +44,9 @@ buildGoModule rec {
   meta = {
     description = "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts";
     homepage = "https://github.com/moul/assh";
-    changelog = "https://github.com/moul/assh/releases/tag/v${version}";
+    changelog = "https://github.com/moul/assh/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = [ ];
     platforms = with lib.platforms; linux ++ darwin;
   };
-}
+})
