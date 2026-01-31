0d9894bac05fc8ac056f4170c9ba9bb0391cf59c
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-31 03:55:47 +0000

    nu_scripts: 0-unstable-2026-01-23 -> 0-unstable-2026-01-29

--- pkgs/by-name/nu/nu_scripts/package.nix
+++ pkgs/by-name/nu/nu_scripts/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation {
   pname = "nu_scripts";
-  version = "0-unstable-2026-01-23";
+  version = "0-unstable-2026-01-29";
 
   src = fetchFromGitHub {
     owner = "nushell";
     repo = "nu_scripts";
-    rev = "e65d76cd14979937ee378d56fc21f4e72f21cecc";
-    hash = "sha256-LPwe0DS7rNFY6MUcfCCqMYJjPkhHQyBmy4M+KjrNz34=";
+    rev = "049b00e54e0dc878bc9a37ee0e05c6ba18789c2f";
+    hash = "sha256-zHLVDV7RsQMMgYP7ysWF83Q+7PoP4E+pOguzixWf9iA=";
   };
 
   installPhase = ''
