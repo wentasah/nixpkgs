ddaf943090e796679165db6b9cd457d503d27918
Dietrich Daroch <Dietrich@Daroch.me>
2026-02-01 07:49:19 -0300

    qsyncthingtray: remove
    Project was last updated in June 2019. Installing this requires allowing the
    insecure package `qtwebengine-5.15.19` which is also quite dated.
    
    https://github.com/sieren/QSyncthingTray
    
    Followed instructions on :/pkgs/README.md

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11085,8 +11085,6 @@ with pkgs;
 
   qsstv = qt5.callPackage ../applications/radio/qsstv { };
 
-  qsyncthingtray = libsForQt5.callPackage ../applications/misc/qsyncthingtray { };
-
   qsudo = libsForQt5.callPackage ../applications/misc/qsudo { };
 
   qtbitcointrader = libsForQt5.callPackage ../applications/misc/qtbitcointrader { };
