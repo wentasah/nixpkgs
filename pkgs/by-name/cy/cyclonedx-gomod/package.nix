3f35dc717abaff81b2d5cdb04aee3806fde3f8ee
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-01 02:00:34 +0100

    cyclonedx-gomod: migrate to finalAttrs

--- pkgs/by-name/cy/cyclonedx-gomod/package.nix
+++ pkgs/by-name/cy/cyclonedx-gomod/package.nix
@@ -4,14 +4,14 @@
   fetchFromGitHub,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "cyclonedx-gomod";
   version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "CycloneDX";
     repo = "cyclonedx-gomod";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-x0/Sei1uY8dDTYCFOgI2e6pe/5BjXLgt8YKKvSMBG4I=";
   };
 
@@ -28,9 +28,9 @@ buildGoModule rec {
   meta = {
     description = "Tool to create CycloneDX Software Bill of Materials (SBOM) from Go modules";
     homepage = "https://github.com/CycloneDX/cyclonedx-gomod";
-    changelog = "https://github.com/CycloneDX/cyclonedx-gomod/releases/tag/v${version}";
+    changelog = "https://github.com/CycloneDX/cyclonedx-gomod/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "cyclonedx-gomod";
   };
-}
+})
