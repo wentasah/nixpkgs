08a777c99a55c491e268d3e26310322180309185
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-01 02:32:00 +0100

    python313Packages.polyswarm-api: migrate to finalAttrs

--- pkgs/development/python-modules/polyswarm-api/default.nix
+++ pkgs/development/python-modules/polyswarm-api/default.nix
@@ -11,7 +11,7 @@
   vcrpy,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "polyswarm-api";
   version = "3.16.0";
   pyproject = true;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "polyswarm";
     repo = "polyswarm-api";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-mdsgHwbGThy2Lzvgzb0mItwJkNspLiqGZzBGGuQdatM=";
   };
 
@@ -42,8 +42,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library to interface with the PolySwarm consumer APIs";
     homepage = "https://github.com/polyswarm/polyswarm-api";
-    changelog = "https://github.com/polyswarm/polyswarm-api/releases/tag/${src.tag}";
+    changelog = "https://github.com/polyswarm/polyswarm-api/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
