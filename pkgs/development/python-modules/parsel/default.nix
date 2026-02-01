00e386641b00755a646cfc1e67f57d2187371ce4
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-01 02:20:56 +0100

    python313Packages.parsel: 1.10.0 -> 1.11.0
    Changelog: https://github.com/scrapy/parsel/blob/v1.11.0/NEWS

--- pkgs/development/python-modules/parsel/default.nix
+++ pkgs/development/python-modules/parsel/default.nix
@@ -3,26 +3,27 @@
   buildPythonPackage,
   cssselect,
   fetchPypi,
+  hatchling,
   jmespath,
   lxml,
   packaging,
   psutil,
   pytestCheckHook,
-  setuptools,
+  sybil,
   w3lib,
 }:
 
 buildPythonPackage rec {
   pname = "parsel";
-  version = "1.10.0";
+  version = "1.11.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-FPF9uVWfUbQzV7nf5DzshwqO+16khXq7Yk7G/4DYoIA=";
+    hash = "sha256-WSX+CH6xb8QEp+2R4x4sHiqbIw2ktk802BNYwNDifog=";
   };
 
-  build-system = [ setuptools ];
+  build-system = [ hatchling ];
 
   dependencies = [
     cssselect
@@ -35,6 +36,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     psutil
     pytestCheckHook
+    sybil
   ];
 
   pythonImportsCheck = [ "parsel" ];
