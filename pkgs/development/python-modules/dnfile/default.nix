c513773775a162d52e97adfc088f4d9b4a1c8e94
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-02 01:07:09 +0100

    python313Packages.dnfile: migrate to finalAttrs

--- pkgs/development/python-modules/dnfile/default.nix
+++ pkgs/development/python-modules/dnfile/default.nix
@@ -7,7 +7,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "dnfile";
   version = "0.18.0";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "malwarefrank";
     repo = "dnfile";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-lIhhiOtMZnYziGeLUK7awJSibP3k8JCYg43jvIl5Puw=";
     fetchSubmodules = true;
   };
@@ -31,8 +31,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module to parse .NET executable files";
     homepage = "https://github.com/malwarefrank/dnfile";
-    changelog = "https://github.com/malwarefrank/dnfile/blob/${src.tag}/HISTORY.rst";
+    changelog = "https://github.com/malwarefrank/dnfile/blob/${finalAttrs.src.tag}/HISTORY.rst";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
