006b8ea7accb818a0bc21bf238dc2f54abc589f8
William Fish <william.michael.fish@gmail.com>
2026-02-02 17:22:37 +0000

    variety: install missing icon to hicolor icon theme

--- pkgs/by-name/va/variety/package.nix
+++ pkgs/by-name/va/variety/package.nix
@@ -92,6 +92,9 @@ python3Packages.buildPythonApplication rec {
   postInstall = ''
     mkdir -p $out/share/applications
     intltool-merge --desktop-style po variety.desktop.in $out/share/applications/variety.desktop
+
+    mkdir -p $out/share/icons/hicolor/scalable/apps
+    cp variety/data/icons/scalable/apps/variety.svg $out/share/icons/hicolor/scalable/apps/variety.svg
   '';
 
   pythonImportsCheck = [ "variety" ];
