4a15f1f1223473fe499871c46683c20a34691f95
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-04 09:02:27 +0100

    python313Packages.heatzypy: modernize

--- pkgs/development/python-modules/heatzypy/default.nix
+++ pkgs/development/python-modules/heatzypy/default.nix
@@ -3,22 +3,19 @@
   aiohttp,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
   setuptools,
   setuptools-scm,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "heatzypy";
   version = "2.5.9";
   pyproject = true;
 
-  disabled = pythonOlder "3.11";
-
   src = fetchFromGitHub {
     owner = "Cyr-ius";
     repo = "heatzypy";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-6vdzxQGNQSMCFYA/nQ2T72RUWmBRvb9v0YcxVbjtG94=";
   };
 
@@ -37,8 +34,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module to interact with Heatzy devices";
     homepage = "https://github.com/Cyr-ius/heatzypy";
-    changelog = "https://github.com/cyr-ius/heatzypy/releases/tag/${version}";
+    changelog = "https://github.com/cyr-ius/heatzypy/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
