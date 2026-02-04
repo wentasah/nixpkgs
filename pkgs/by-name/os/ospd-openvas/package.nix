19d1552f3b35242c5b838be4eb8df7fdd8666939
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-04 18:39:43 +0100

    ospd-openvas: modernize

--- pkgs/by-name/os/ospd-openvas/package.nix
+++ pkgs/by-name/os/ospd-openvas/package.nix
@@ -4,7 +4,7 @@
   python3,
 }:
 
-python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "ospd-openvas";
   version = "22.10.1";
   pyproject = true;
@@ -12,7 +12,7 @@ python3.pkgs.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "greenbone";
     repo = "ospd-openvas";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-YrCYudM45xDSQRNhUfYAxFNKToMsWdGwtDYJiZ0E6+c=";
   };
 
@@ -26,7 +26,7 @@ python3.pkgs.buildPythonApplication rec {
 
   build-system = with python3.pkgs; [ poetry-core ];
 
-  propagatedBuildInputs = with python3.pkgs; [
+  dependencies = with python3.pkgs; [
     defusedxml
     deprecated
     lxml
@@ -45,9 +45,9 @@ python3.pkgs.buildPythonApplication rec {
   meta = {
     description = "OSP server implementation to allow GVM to remotely control an OpenVAS Scanner";
     homepage = "https://github.com/greenbone/ospd-openvas";
-    changelog = "https://github.com/greenbone/ospd-openvas/releases/tag/${src.tag}";
+    changelog = "https://github.com/greenbone/ospd-openvas/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.agpl3Only;
     maintainers = with lib.maintainers; [ fab ];
     platforms = lib.platforms.linux;
   };
-}
+})
