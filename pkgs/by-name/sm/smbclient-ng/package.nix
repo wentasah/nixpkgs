0ddf2f6330b2b29a264f8192d2b11bcf72844210
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-04 23:21:46 +0100

    smbclient-ng: migrate to finalAttrs

--- pkgs/by-name/sm/smbclient-ng/package.nix
+++ pkgs/by-name/sm/smbclient-ng/package.nix
@@ -4,7 +4,7 @@
   python3,
 }:
 
-python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "smbclient-ng";
   version = "3.0.0";
   pyproject = true;
@@ -12,7 +12,7 @@ python3.pkgs.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "p0dalirius";
     repo = "smbclient-ng";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-W0f+PxPjI5raIjUNK7fcfPvFugrJxLZTWZPpX/6P56w=";
   };
 
@@ -39,9 +39,9 @@ python3.pkgs.buildPythonApplication rec {
   meta = {
     description = "Tool to interact with SMB shares";
     homepage = "https://github.com/p0dalirius/smbclient-ng";
-    changelog = "https://github.com/p0dalirius/smbclient-ng/releases/tag/${version}";
+    changelog = "https://github.com/p0dalirius/smbclient-ng/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "smbclientng";
   };
-}
+})
