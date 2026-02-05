d69e2c493b3c736f194580f374bbc57ff957da65
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-05 09:15:24 +0100

    python314Packages.nbdev: migrate to finalAttrs

--- pkgs/development/python-modules/nbdev/default.nix
+++ pkgs/development/python-modules/nbdev/default.nix
@@ -14,13 +14,13 @@
   pyyaml,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "nbdev";
   version = "3.0.10";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-5NPOctZ68qS7ETEr/zwRY9Q66gjVa3F7gyc7cssyI4A=";
   };
 
@@ -50,8 +50,8 @@ buildPythonPackage rec {
   meta = {
     homepage = "https://github.com/fastai/nbdev";
     description = "Create delightful software with Jupyter Notebooks";
-    changelog = "https://github.com/fastai/nbdev/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/fastai/nbdev/blob/${finalAttrs.version}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ rxiao ];
   };
-}
+})
